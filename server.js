7306dc1ad46adab7ba101580d7a828171f8e0577
User <user@UserdeMacBook-Pro.local>
2021-11-14 20:59:31 +0800

    add login POST route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 const express = require('express');
 // create an instance of the express constructor will name "app".
 const app = express();
+// use bodyparser library for POST action
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 
 // create some mock data in Json
 const mockUserData = [
@@ -28,6 +32,26 @@ app.get('/users/:id',function(req,res){
 	})
 })
 
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
+})
 
 // make a server by including their name after app.
 app.listen(8000, function () {
